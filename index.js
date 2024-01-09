221a127d7b1f4d6cfb3dd8d278ad484c63ef958a
Bruce-Mahagwa <96821698+Bruce-Mahagwa@users.noreply.github.com>
2024-01-09 10:03:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const PORT = 3001;
 const app = express();
 //  middleware
 app.use(express.json());
-app.use(cors({  credentials: true, origin: "https://upworks2.vercel.app" }));
+app.use(cors({  credentials: true, origin: "https://upworks-backend.onrender.com" }));
 app.use(cookieParser())
 app.use("/uploads", express.static(__dirname + "/uploads"))
 // begin of routes
